294c6919b7d9d74facd37df38071c31079efe229
166187 <lakshya.dewan@axisbank.com>
2020-10-05 08:18:09 +0530

    add array interger missing | 20-Array

---
+++ src/main/java/array/mustDoArray/MissingNoIntegerArray.java
@@ -0,0 +1,26 @@
+package array.mustDoArray;
+
+import java.util.Arrays;
+
+public class MissingNoIntegerArray {
+
+
+    public static void main(String[] args) {
+
+        int[] arra = new int[]{-3, -8, 2, 3, 4, 1, 2, 3, 8, 9};
+        int[] register = new int[arra.length];
+
+        for (int i : arra) {
+            if (i > 0) {
+                register[i] = 1;
+            }
+        }
+        System.out.println("length of array is " + arra.length);
+        for (int i = 0; i < arra.length; i++) {
+            if (register[i] == 0) {
+                System.out.println("missing element is " + (i));
+            }
+        }
+        System.out.println(Arrays.toString(register));
+    }
+}
